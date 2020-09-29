b378ad0a25f907ad3315a231ff596ad0a15a88e3
kiesuch <kiesuch@aol.com>
2020-09-29 18:25:42 -0400

    Created forbidden file

---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Forbidden = () => (
+	<div className="bounds">
+		<h1>Forbidden</h1>
+		<p>Oh oh! You can't access this page.</p>
+		<a className="button" href="/">Return to main page</a>
+	</div>
+)
+
+export default Forbidden;
